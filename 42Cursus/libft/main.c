5b64dddc5a4b8fd842029320b8ba06e113fbb3af
Michhern <150728190+Michhern@users.noreply.github.com>
2024-03-19 16:22:14 +0100

    subo el main.c del printf

---
+++ 42Cursus/libft/main.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: michhern <michhern@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/06 17:47:33 by michhern          #+#    #+#             */
+/*   Updated: 2024/03/19 16:10:17 by michhern         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+/*int	main(void)
+{
+	int	number = -3456789;
+
+	printf("Number: %d\n", number);
+	ft_putnbr(number);
+	return (0);
+}*/
+
+/*int	main(void)
+{
+	char	*messange = "Hello, world!";
+
+	ft_putstr(messange);
+	return (0);
+}*/
+
+/*int main(void)
+{
+	int	len = 0;
+	// Prueba con diferentes valores
+	printf("Hexadecimal de 0: %X\n", 0);
+	ft_hexadecimal(0, &len, 'X');
+		printf("\n");
+	printf("Hexadecimal de 1: %X\n", 1);
+	ft_hexadecimal(1, &len, 'X');
+		printf("\n");
+	printf("Hexadecimal de 16: %X\n", 16);
+	ft_hexadecimal(16, &len, 'X');
+	printf("\n");
+	printf("Hexadecimal de 123: %X\n", 123);
+	ft_hexadecimal(123, &len, 'X');
+	printf("\n");
+	printf("Hexadecimal de -1: %X\n", -1);
+	ft_hexadecimal(-1, &len, 'X');
+	printf("\n");
+	printf("Hexadecimal de -123: %X\n", -123);
+	ft_hexadecimal(-123, &len, 'X');
+	printf("\n");
+	printf("Hexadecimal de -2: %X\n", -2);
+	ft_hexadecimal(-2, &len, 'X');
+	printf("\n");
+	printf("Hexadecimal de 255: %X\n", 255);
+	ft_hexadecimal(255, &len, 'X');
+	printf("\n");
+	printf("Hexadecimal de -255: %X \n", -255);
+	ft_hexadecimal(-255, &len, 'X');
+	printf("\n");
+	return 0;
+}*/
+/*int	main(void)
+{
+	// Test con %c
+	ft_printf("Test con %%c: %c\n", 'A');
+	printf("Test con %%c: %c\n", 'A');
+
+	// Test con %s
+	//ft_printf("Test con %%s: %s\n", "Hola, mundo!");
+	//printf("Test con %%s: %s\n", "Hola, mundo!");
+
+	// Test con %d
+	ft_printf("Test con %%d: %d\n", 12345);
+	printf("Test con %%d: %d\n", 12345);
+
+	// Test con %x
+	ft_printf("Test con %%x: %x\n", 255);
+	printf("Test con %%x: %x\n", 255);
+
+	// Test con %p
+	int *ptr = NULL;
+	ft_printf("Test con %%p: %p\n", (void *)ptr);
+	printf("Test con %%p: %p\n", (void *)ptr);
+
+	// Test con %%
+	ft_printf("Test con %%%%: %%\n");
+	printf("Test con %%%%: %%\n");
+
+	return 0;
+}*/
