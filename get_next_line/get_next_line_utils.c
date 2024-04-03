2932352f86a808ba9a1abe1f86f17ca9b0bb1b02
Michhern <150728190+Michhern@users.noreply.github.com>
2024-04-03 13:48:19 +0200

    add get

---
+++ get_next_line/get_next_line_utils.c
@@ -0,0 +1,119 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: michhern <michhern@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/27 12:06:39 by michhern          #+#    #+#             */
+/*   Updated: 2024/04/03 13:47:37 by michhern         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int	found_newline(t_list *list)
+{
+	int	i;
+
+	if (list == NULL)
+		return (0);
+	while (list)
+	{
+		i = 0;
+		while (list->str_buf[i] && i < BUFFER_SIZE)
+		{
+			if (list->str_buf[i] == '\n')
+				return (1);
+			i++;
+		}
+		list = list->next;
+	}
+	return (0);
+}
+
+t_list	*find_last_node(t_list *list)
+{
+	if (list == NULL)
+		return (NULL);
+	while (list->next)
+		list = list->next;
+	return (list);
+}
+
+void	str_cpy(t_list *list, char *str)
+{
+	int	i;
+	int	c;
+
+	if (list == NULL)
+		return ;
+	c = 0;
+	while (list)
+	{
+		i = 0;
+		while (list->str_buf[i])
+		{
+			if (list->str_buf[i] == '\n')
+			{
+				str[c++] = '\n';
+				str[c] = '\0';
+				return ;
+			}
+			str[c++] = list->str_buf[i++];
+		}
+		list = list->next;
+	}
+	str[c] = '\0';
+}
+
+int	len_to_newline(t_list *list)
+{
+	int	i;
+	int	len;
+
+	if (list == NULL)
+		return (0);
+	len = 0;
+	while (list)
+	{
+		i = 0;
+		while (list->str_buf[i])
+		{
+			if (list->str_buf[i] == '\n')
+			{
+				++len;
+				return (len);
+			}
+			++i;
+			++len;
+		}
+		list = list->next;
+	}
+	return (len);
+}
+
+void	free_malloc(t_list **list, t_list *free_node, char *buf)
+{
+	t_list	*tmp;
+
+	if (*list == NULL)
+		return ;
+	while (*list)
+	{
+		tmp = (*list)->next;
+		free((*list)->str_buf);
+		free(*list);
+		*list = tmp;
+	}
+	*list = NULL;
+	if (free_node && free_node->str_buf[0])
+		*list = free_node;
+	else
+	{
+		if (buf != NULL)
+			free(buf);
+		if (free_node != NULL)
+			free(free_node);
+	}
+}
