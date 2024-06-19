9385b901a58745831b55d0acd81690cfd30188bd
Michhern <150728190+Michhern@users.noreply.github.com>
2024-06-19 12:30:17 +0200

    Update search_and_replace.c

--- testgrademe/search_and_replace/search_and_replace.c
+++ testgrademe/search_and_replace/search_and_replace.c
@@ -14,34 +14,17 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int	ft_strlen(char *str)
+int	main(int ac, char **av)
 {
-	int	len;
-
-	len = 0;
-	while (str[len])
-		len++;
-	return (len);
-}
-
-int	main(int argc, char **argv)
-{
-	int	i;
-
-	if (argc != 4)
+	if (ac == 4 && av[2][1] == '\0' && av[3][1] == '\0')
 	{
-		write(1, "\n", 1);
-		exit (0);
-	}
-	if (ft_strlen(argv[2]) == 1 && ft_strlen(argv[3]) == 1)
-	{
-		i = 0;
-		while (argv[1][i])
+		while (*av[1])
 		{
-			if (argv[1][i] == argv[2][0])
-				argv[1][i] = argv[3][0];
-			write(1, &argv[1][i], 1);
-			i++;
+			if (*av[2] == *av[1])
+				write(1, av[3], 1);
+			else
+				write(1, av[1], 1);
+			av[1]++;
 		}
 	}
 	write(1, "\n", 1);
